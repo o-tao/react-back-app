553d69356101c6210375daed3e4d49893e976c93
o-tao <dhtkdals369@gmail.com>
2024-09-30 10:59:20 +0900

    front 서버와 연동

--- src/main/java/com/app/reactbackapp/controller/HomeController.java
+++ src/main/java/com/app/reactbackapp/controller/HomeController.java
@@ -1,16 +1,29 @@
 package com.app.reactbackapp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String index() {
         return "Server Start!";
+    }
+
+    @GetMapping("/form1")
+    public Map<String, String> form1(@RequestParam("name") String name,
+                                     @RequestParam("password") String password) {
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("name", name);
+        resultMap.put("password", password);
+
+        return resultMap;
     }
 }

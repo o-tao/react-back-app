f4cf9ae091a67fed76aff09464f647fecc3bf2f2
o-tao <dhtkdals369@gmail.com>
2024-09-30 10:46:57 +0900

    react-app에 사용되는 back 프로젝트 생성

---
+++ src/main/java/com/app/reactbackapp/ReactBackAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.reactbackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactBackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/reactbackapp/ReactBackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.reactbackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

1636b0ad5320ce7d18dcf5497b623d08a8f85bf3
o-tao <dhtkdals369@gmail.com>
2024-09-30 11:12:20 +0900

    PostMapping 추가

--- src/main/java/com/app/reactbackapp/controller/HomeController.java
+++ src/main/java/com/app/reactbackapp/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.app.reactbackapp.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,4 +23,15 @@ public class HomeController {
 
         return resultMap;
     }
+
+    @PostMapping("/form1")
+    public Map<String, String> form2(@RequestParam("name") String name,
+                                     @RequestParam("password") String password) {
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("name", name);
+        resultMap.put("password", password);
+
+        return resultMap;
+    }
+
 }

8309ae7e8206b67d4a8cb07b5f34faf4fcff3c45
o-tao <dhtkdals369@gmail.com>
2024-09-30 10:52:17 +0900

    네이밍 변경

--- src/main/java/com/app/reactbackapp/controller/HomeController.java
+++ src/main/java/com/app/reactbackapp/controller/HomeController.java
@@ -3,11 +3,14 @@ package com.app.reactbackapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-public class Controller {
+public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String home() {
         return "Server Start!";
     }
 }

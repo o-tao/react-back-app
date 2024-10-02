31c8854db3a3d59fad300a878b0fa456c85be763
o-tao <dhtkdals369@gmail.com>
2024-10-02 12:58:49 +0900

    react-app ViewApp data start, end 처리

--- src/main/java/com/app/reactbackapp/controller/DataController.java
+++ src/main/java/com/app/reactbackapp/controller/DataController.java
@@ -15,26 +15,30 @@ public class DataController {
     public Map<String, Object> Data(@RequestParam("page") String page) {
         Map<String, Object> resultMap = new HashMap<>();
         ArrayList<Map<String, Object>> array;
+        int start = 0;
         int end = 0;
         if ("1".equals(page)) {
+            start = 1;
             end = 5;
         }
         if ("2".equals(page)) {
+            start = 6;
             end = 10;
         }
         if ("3".equals(page)) {
+            start = 11;
             end = 15;
         }
-        array = getData(end);
+        array = getData(start, end);
         resultMap.put("page", Integer.parseInt(page));
         resultMap.put("results", array);
         resultMap.put("total_pages", 3);
         return resultMap;
     }
 
-    private ArrayList<Map<String, Object>> getData(int end) {
+    private ArrayList<Map<String, Object>> getData(int start, int end) {
         ArrayList<Map<String, Object>> array = new ArrayList<>();
-        for (int i = 1; i <= 5; i++) {
+        for (int i = start; i <= end; i++) {
             Map<String, Object> map = new HashMap<>();
             map.put("id", i);
             map.put("name", "data" + i);

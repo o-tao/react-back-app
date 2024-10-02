f8300050e7fc828dda237e3763414000aefd802b
o-tao <dhtkdals369@gmail.com>
2024-10-02 12:45:42 +0900

    react-app ViewApp data.json 전송1ˆ

--- src/main/java/com/app/reactbackapp/controller/DataController.java
+++ src/main/java/com/app/reactbackapp/controller/DataController.java
@@ -2,6 +2,8 @@ package com.app.reactbackapp.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -11,7 +13,19 @@ public class DataController {
 
     @GetMapping
     public Map<String, Object> Data(@RequestParam("page") String page) {
-        System.out.println(page);
-        return null;
+        Map<String, Object> resultMap = new HashMap<>();
+        ArrayList<Map<String, Object>> array = new ArrayList<>();
+        if ("1".equals(page)) {
+            for (int i = 1; i <= 5; i++) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", i);
+                map.put("name", "data" + i);
+                array.add(map);
+            }
+        }
+        resultMap.put("page", page);
+        resultMap.put("results", array);
+        resultMap.put("total_pages", 3);
+        return resultMap;
     }
 }
